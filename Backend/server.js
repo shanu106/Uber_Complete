4c55721f8f035813bb12f79f1199a6b78fdb667d
Shahnawaj Rangrej <SHAHNAWAJNILGER244@GMAIL.COM>
2025-01-09 15:43:29 +0530

    Update server.js
    or in port

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
 const app = require('./app');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const {initializeSocket } = require('./socket');
 
 const server = http.createServer(app);
 initializeSocket(server)
 
 
-server.listen(5000, ()=>{
+server.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
     
 })
