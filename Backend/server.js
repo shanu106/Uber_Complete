6688a20be6798fc0f4e3bf740219c229f194d5e0
Shahnawaj Rangrej <SHAHNAWAJNILGER244@GMAIL.COM>
2025-01-09 15:38:32 +0530

    Update server.js
    port = 5000

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 initializeSocket(server)
 
 
-server.listen(port, ()=>{
+server.listen(5000, ()=>{
     console.log(`server is running on port ${port}`);
     
 })
